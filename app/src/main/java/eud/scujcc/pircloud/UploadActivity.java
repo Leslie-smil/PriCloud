c6219b4351cc7259dc86a7d7d1dd28ead94f69a4
zeke <zzhmisanmia@outlook.com>
2020-06-08 14:15:39 +0800

    修复了bug

--- app/src/main/java/eud/scujcc/pircloud/UploadActivity.java
+++ app/src/main/java/eud/scujcc/pircloud/UploadActivity.java
@@ -2,11 +2,7 @@ package eud.scujcc.pircloud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
